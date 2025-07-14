1f866fda5e133bed41146a01e2db75a3c586d516
flavionascimento99 <flaviohnrq879@gmail.com>
2025-07-14 18:13:24 -0300

    testing mapping

---
+++ src/main/java/io/nascimento/spring_playground/resources/UserResource.java
@@ -0,0 +1,18 @@
+package io.nascimento.spring_playground.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.nascimento.spring_playground.entities.User;
+
+@RestController
+@RequestMapping(value="/Users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User exampleUser = new User(1L, "Flavio", "flavio@mail.com", "99 9 99999999", "senhaSegura123");
+		return ResponseEntity.ok().body(exampleUser);
+	}
+}
